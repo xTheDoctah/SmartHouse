0ff3de0d879fa9e1e45ea86bb76df134f9de8ae9
xTheDoctah <alessio.caldarigi@gmail.com>
2018-05-30 20:30:21 +0200

    A river of code!

--- SmartHouseClient/README.md
+++ SmartHouseClient/README.md
@@ -1,6 +1,6 @@
 # SmartHouseClient
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.5.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.3.
 
 ## Development server
 
--- SmartHouseClient/src/app/app.module.ts
+++ SmartHouseClient/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClient,HttpClientModule} from '@angular/common/http'
 import { AppComponent } from './app.component';
+import { MeteoComponent } from './meteo/meteo.component';
+import { TemperatureComponent } from './temperature/temperature.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeteoComponent,
+    TemperatureComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
